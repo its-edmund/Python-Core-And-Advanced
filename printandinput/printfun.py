c5c1a6f465d0b32757dac68f3968be415ece90eb
Edmund Xin <edmundxin@Edmunds-MacBook-Air.local>
2020-04-05 13:43:09 -0700

    Finish print and input

---
+++ printandinput/printfun.py
@@ -0,0 +1,14 @@
+print()
+print("Hello"*3)
+print("All the power \n is within you")
+
+a,b=10,20
+print(a,b,sep='+++')
+
+name="Johnny"
+grades=95.3153
+
+print("Name is",name,"Grades are",grades)
+print("Name is %s,Grades are %.2f"%(name,grades))
+
+print("Name is {},Grades are {}".format(name,grades))
