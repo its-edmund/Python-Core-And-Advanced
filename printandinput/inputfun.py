e55ebcc0b8d8bab79ce7571736126ff9f44daac2
Edmund Xin <edmundxin@Edmunds-MacBook-Air.local>
2020-04-05 13:43:48 -0700

    Finish print and input

--- printandinput/inputfun.py
+++ printandinput/inputfun.py
@@ -1,1 +1,9 @@
-input()
+"""s=input()
+print(s)
+s=input("Enter your name:")
+print(s)"""
+# i=int(input("Enter an integer number:"))
+# print(type(i))
+
+lst = [float(x) for x in input('Enter three numbers separated by comma:').split(',')]
+print(lst)
