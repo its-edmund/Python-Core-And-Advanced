1d77d3750bc98e9601389d82b18ad9ab328c4e2c
Edmund Xin <edmundxin@Edmunds-MacBook-Air.local>
2020-04-05 11:15:19 -0700

    Finish Operators

---
+++ operators/logicaloperators.py
@@ -0,0 +1,8 @@
+x=20
+y=30
+
+print(x==20 and y==30)      # True
+print(x==5 and y==30)       # False
+print(x==5 or y==30)        # True
+print(x==5 or y==23)        # False
+print(not(x==5 or y==23))   # True
